09e0d27fc5e8a83da4a2a1b8994f90343b3cd78d
laijasmine <jasmine.lai@alumni.ubc.ca>
2019-09-26 23:36:34 -0700

    adding introduction material skeleton

---
+++ lessons/01_intro.R
@@ -0,0 +1,43 @@
+# Getting set up & oriented to Rstudio
+
+
+# R projects .Rproj
+
+
+
+# R vs Rstudio
+
+
+# quick comment on commenting
+
+
+
+# Libraries
+
+
+# Anatomy of a function
+
+
+# Assignment <-
+
+
+
+# Getting help()
+# links
+
+
+# Data types
+# character, number, logical
+# lists, dataframes
+
+## str()
+
+
+
+
+
+# Working Directories
+## Important for reading in data later!
+
+
+
